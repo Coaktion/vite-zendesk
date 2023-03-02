5e9f5b60dfeb2b4d869ea069e2dd6f12b39fb16f
Joao Macedo <joaovictormacedo91@gmail.com>
2023-03-02 16:13:40 -0300

    feat: adding MainApp component

---
+++ src/main-app.tsx
@@ -0,0 +1,13 @@
+import { Sidebar } from './presentation/apps'
+
+const MainApp: React.FC = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const destinationApp = urlParams.get('type') || urlParams.get('modal');
+
+  if (destinationApp === 'sidebar') return <Sidebar />;
+
+  return <div>Not found</div>;
+}
+
+export default MainApp
+
