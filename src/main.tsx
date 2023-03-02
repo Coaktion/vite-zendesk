96484caeb139af422db21e04214e617b3970f06d
Joao Macedo <joaovictormacedo91@gmail.com>
2023-03-02 18:14:59 -0300

    style: add global styles and colors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MainApp from './main-app'
+import './presentation/styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
