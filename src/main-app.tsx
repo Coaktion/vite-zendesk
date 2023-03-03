587ee5ae972fcd485c4dede75efc0ebe0370d445
Joao Macedo <joaovictormacedo91@gmail.com>
2023-03-03 11:10:01 -0300

    feat: add zendesk as props

--- src/main-app.tsx
+++ src/main-app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Sidebar} from './presentation/apps';
 
-const MainApp: React.FC = () => {
+type Props = {
+	zendesk: any;
+};
+
+const MainApp: React.FC<Props> = ({zendesk}: Props) => {
 	const urlParams = new URLSearchParams(window.location.search);
 	const destinationApp = urlParams.get('type') ?? urlParams.get('modal');
 
-	if (destinationApp === 'sidebar') {
-		return <Sidebar />;
-	}
+	if (destinationApp === 'sidebar') return <Sidebar zendesk={zendesk} />;
 
 	return <div>Not found</div>;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ZAFClient from 'zendesk_app_framework_sdk';
 import MainApp from './main-app';
 import './presentation/styles/global.scss';
 import './presentation/translations';
 
+const zendesk = ZAFClient.init();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<MainApp />
+		<MainApp zendesk={zendesk} />
 	</React.StrictMode>,
 );
