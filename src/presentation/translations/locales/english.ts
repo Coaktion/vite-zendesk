81926ce6fbfcd2d77229fda90728cfd2e412bf78
Joao Macedo <joaovictormacedo91@gmail.com>
2023-03-03 11:09:02 -0300

    chore: add new validation translate texts

--- src/presentation/translations/locales/english.ts
+++ src/presentation/translations/locales/english.ts
@@ -2,6 +2,10 @@ export const english = {
 	presentation: {
 		apps: {
 			sidebar: {
+				validation: {
+					required: 'Inform the username',
+					min: 'Inform at least 3 characters',
+				},
 				button: 'Search',
 				label: 'Inform the GitHub username',
 				'github-user-data': {
--- src/presentation/translations/locales/portuguese.ts
+++ src/presentation/translations/locales/portuguese.ts
@@ -2,6 +2,10 @@ export const portuguese = {
 	presentation: {
 		apps: {
 			sidebar: {
+				validation: {
+					required: 'Informe o usuário',
+					min: 'Informe no mínimo 3 caracteres',
+				},
 				button: 'Buscar',
 				label: 'Informe o nome do usuário GitHub',
 				'github-user-data': {
