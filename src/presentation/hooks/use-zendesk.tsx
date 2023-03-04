15993372cf6c3c3c21909609345967112a49029c
Joao Macedo <joaovictormacedo91@gmail.com>
2023-03-04 19:52:01 -0300

    feat: add useZendesk hook

---
+++ src/presentation/hooks/use-zendesk.tsx
@@ -0,0 +1,31 @@
+import React, {createContext, type ReactNode, useContext, useMemo, useState} from 'react';
+import {type Zendesk} from '@/services/zendesk';
+
+type ContextProps = {
+	zendesk: Zendesk;
+	setZendesk: React.Dispatch<React.SetStateAction<Zendesk>>;
+};
+
+type Props = {
+	children: ReactNode;
+};
+
+const ZendeskContext = createContext<ContextProps>({} as ContextProps);
+
+const ZendeskProvider: React.FC<Props> = ({children}: Props) => {
+	const [zendesk, setZendesk] = useState<Zendesk>({} as Zendesk);
+	const contextValue = useMemo(() => ({zendesk, setZendesk}), [zendesk, setZendesk]);
+	return (
+		<ZendeskContext.Provider value={contextValue}>
+			{children}
+		</ZendeskContext.Provider>
+	);
+};
+
+const useZendesk = (): ContextProps => {
+	const context = useContext(ZendeskContext);
+	if (!context) throw new Error('useZendesk must be used within a ZendeskProvider');
+	return context;
+};
+
+export {ZendeskProvider, useZendesk};
