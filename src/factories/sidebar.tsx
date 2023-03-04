06e1e098515d63009cf183ae8d6960be170d1f30
Joao Macedo <joaovictormacedo91@gmail.com>
2023-03-04 19:53:58 -0300

    feat: add Sidebar component factory

---
+++ src/factories/sidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Sidebar} from '@/presentation/apps';
+import {useZendesk} from '@/presentation/hooks/use-zendesk';
+import {GithubClient} from '@/services/github-client';
+
+export const MakeSidebar: React.FC = () => {
+	const {zendesk} = useZendesk();
+	const githubClient = new GithubClient(String(zendesk?._settings.base_url));
+	return <Sidebar zendesk={zendesk} githubClient={githubClient} />;
+};
