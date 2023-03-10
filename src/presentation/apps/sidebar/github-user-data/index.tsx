f5e94147fa62b621b91b8f8041ae927c141e4bd9
Joao Macedo <joaovictormacedo91@gmail.com>
2023-03-10 19:45:11 -0300

    test: add GithubUserData component test

--- src/presentation/apps/sidebar/github-user-data/index.tsx
+++ src/presentation/apps/sidebar/github-user-data/index.tsx
@@ -1,23 +1,31 @@
 import React, {useEffect} from 'react';
 import {useTranslation} from 'react-i18next';
 import {GroupAdd, Group, Badge, LocationCity, ArrowBack} from '@mui/icons-material';
-import {type SidebarState} from '@/interfaces';
+import {type Settings, type SidebarState} from '@/interfaces';
 import './github-user-data.scss';
 import {type ZendeskTicketsClient} from '@coaktion/zendesk-clients-react';
 
 type Props = {
 	sidebarState: SidebarState;
-	goBack: () => void;
+	setState: React.Dispatch<React.SetStateAction<SidebarState>>;
 	zendesk: ZendeskTicketsClient;
+	settings: Settings;
 };
 
-const GithubUserData: React.FC<Props> = ({sidebarState, goBack, zendesk}: Props) => {
+const GithubUserData: React.FC<Props> = ({
+	sidebarState, setState, zendesk, settings}: Props,
+) => {
 	const {t} = useTranslation();
 	const {githubUserData: {user, repositories}} = sidebarState;
 
+	const goBack = (): void => {
+		zendesk.resizeFrame();
+		setState(current => ({...current, userFound: false, githubUser: ''}));
+	};
+
 	const handleRepository = (description: string, language: string): void => {
-		// Zendesk.setTicketField(zendesk._settings.repo_description_id, description);
-		// zendesk.setTicketField(zendesk._settings.repo_language_id, language);
+		zendesk.setTicketField(settings.repo_description_id, description);
+		zendesk.setTicketField(settings.repo_language_id, language);
 		zendesk.notifyUser(
 			t('presentation.apps.sidebar.github-user-data.success'),
 			'success',
@@ -29,8 +37,8 @@ const GithubUserData: React.FC<Props> = ({sidebarState, goBack, zendesk}: Props)
 	}, []);
 
 	return (
-		<div className='githubUserDataWrap'>
-			<div className='dataWrap' onClick={goBack}>
+		<div data-testid='githubUserDataWrap' className='githubUserDataWrap'>
+			<div data-testid='github-user-data-goback' className='dataWrap' onClick={goBack}>
 				<span className='goBack'>
 					<ArrowBack />
 					{t('presentation.apps.sidebar.github-user-data.goback-button')}
@@ -40,15 +48,16 @@ const GithubUserData: React.FC<Props> = ({sidebarState, goBack, zendesk}: Props)
 			<img
 				src={user.avatar_url}
 				alt={user.name}
+				data-testid='github-user-data-avatar-img'
 			/>
 
 			<div className='dataWrap'>
-				<span>
+				<span data-testid='github-user-data-followers'>
 					<GroupAdd />
 					{t('presentation.apps.sidebar.github-user-data.followers')}:
 					<b>{user.followers}</b>
 				</span>
-				<span>
+				<span data-testid='github-user-data-following'>
 					<Group />
 					{t('presentation.apps.sidebar.github-user-data.following')}:
 					<b>{user.following}</b>
@@ -56,7 +65,7 @@ const GithubUserData: React.FC<Props> = ({sidebarState, goBack, zendesk}: Props)
 			</div>
 
 			<div className='dataWrap'>
-				<span>
+				<span data-testid='github-user-data-name'>
 					<Badge />
 					{t('presentation.apps.sidebar.github-user-data.name')}:
 					<b>{user.name}</b>
@@ -64,16 +73,16 @@ const GithubUserData: React.FC<Props> = ({sidebarState, goBack, zendesk}: Props)
 			</div>
 
 			<div className='dataWrap'>
-				<span>
+				<span data-testid='github-user-data-city'>
 					<LocationCity />
 					{t('presentation.apps.sidebar.github-user-data.city')}:
 					<b>{user.location}</b>
 				</span>
 			</div>
 
-			<div className='divider'/>
+			<div className='divider' data-testid='github-user-data-divider'/>
 
-			<div className='reposWraps'>
+			<div data-testid='github-user-data-repos' className='reposWraps'>
 				{repositories.map(repository => (
 					<div
 						key={repository.name}
